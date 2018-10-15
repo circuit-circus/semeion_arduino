aac234c167f52da1e86fd4df41848c0fa3ea8af3
Jesper Fogh <foghjesperjhf@gmail.com>
2018-10-15 14:47:03 +0200

    Updated the code so that it sends various values through the Serial port

--- semeion_arduino.ino
+++ semeion_arduino.ino
@@ -19,42 +19,55 @@
 // Init new Tact Toolkit
 Tact Tact(TACT_MULTI);
 
-#define SPECTRUMSTART 48
+#define SENSOR_ID 0
+int SPECTRUMSTART = 60;
 #define SPECTRUMSTEPS 32
-#define SPECTRUMSTEPSIZE 2
+#define SPECTRUMSTEPSIZE 1
 
 String spectrumSerial = "";
 
+// This shifts the SpectrumStart by one each time the shift timer runs out.
+// This result in "crashing" after 8 iterations, since the Tact library can only handle 8 sensors. 
+bool shouldShiftStart = false;
+long shiftTimer = 0;
+static long shiftTimerDuration = 7500;
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Start Tact toolkit
   Tact.begin();
 
   // Add new Sensor and config
-  Tact.addSensor(0, SPECTRUMSTART, SPECTRUMSTEPS, SPECTRUMSTEPSIZE);
+  Tact.addSensor(SENSOR_ID, SPECTRUMSTART, SPECTRUMSTEPS, SPECTRUMSTEPSIZE);
+  shiftTimer = millis();
 }
 
 
 void loop() {
+  if(millis() > shiftTimer + shiftTimerDuration && shouldShiftStart) {
+    SPECTRUMSTART += SPECTRUMSTEPS * SPECTRUMSTEPSIZE;
+    Tact.addSensor(SENSOR_ID, SPECTRUMSTART, SPECTRUMSTEPS, SPECTRUMSTEPSIZE);
+    shiftTimer = millis();
+  }
 
-  // read Bias for sensor
-  int bias = Tact.readBias(0);
-
-  // read Peak for first sensor
-  int peak = Tact.readPeak(0);
-
-  // read Spectrum for first sensor
-  // pay attention that the spectrum array can hold 
-  // as many values as you have defined in Tact.addSensor();
+  // read Peak and Bias for sensor
+  int bias = Tact.readBias(SENSOR_ID);
+  int peak = Tact.readPeak(SENSOR_ID);
+  // read Spectrum for sensor
+  // the spectrum array holds as many values as defined in SPECTRUMSTEPS;
   int spectrum[SPECTRUMSTEPS];
-  Tact.readSpectrum(0, spectrum);
-  
-  spectrumSerial = "";
+  Tact.readSpectrum(SENSOR_ID, spectrum);
+
+  spectrumSerial = bias;
+  spectrumSerial = spectrumSerial + ",";
+  spectrumSerial = spectrumSerial + peak;
+  spectrumSerial = spectrumSerial + ",";
+  spectrumSerial = spectrumSerial + SPECTRUMSTART;
+  spectrumSerial = spectrumSerial + ",";
   for(int i = 0; i < SPECTRUMSTEPS; i++) {
-    spectrumSerial = spectrumSerial + spectrum[i];
+    spectrumSerial += spectrum[i];
     if(i < SPECTRUMSTEPS -1) {
       spectrumSerial = spectrumSerial + ",";
     }
