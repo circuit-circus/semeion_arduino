f519d02e482b86d83202c3a656c1985716147886
Jesper Fogh <foghjesperjhf@gmail.com>
2018-10-24 16:29:06 +0200

    Combined the I2C connection code with the Tact sensor code, so that we now send tact sensor data over I2C

--- semeion_arduino.ino
+++ semeion_arduino.ino
@@ -13,26 +13,33 @@ SEMEION ARDUINO
 A CIRCUIT CIRCUS PROJECT 
 */
 
-// Include Tact Library
+// Includes
 #include <Tact.h>
+#include <Wire.h>
 
-// Init new Tact Toolkit
+// Tact Inits
 Tact Tact(TACT_MULTI);
-
 int SPECTRUMSTART = 73;
 const int SENSOR_ID = 0;
 const int SPECTRUMSTEPS = 24;
 const int SPECTRUMSTEPSIZE = 1;
-
 int spectrum[SPECTRUMSTEPS];
 
-// Average values
+// I2C Inits
+#define SLAVE_ADDRESS 0x08
+
+// 10 byte data buffer
+int receiveBuffer[9];
+// A counter to send back
+uint8_t keepCounted = 0;
+
+// Tact - Average values
 const int AVRG_SAMPLE_COUNT = 5;
 int avrgCounterSamples[SPECTRUMSTEPS][AVRG_SAMPLE_COUNT];
 int avrgSampleIndex = 0;
 float movingAvrg = 0.0f;
 
-// Calibration
+// Tact - Calibration
 const int BASELINE_SAMPLE_COUNT = 5;
 float baselineSamples[BASELINE_SAMPLE_COUNT];
 int baselineSampleIndex = 0;
@@ -45,11 +52,58 @@ void setup() {
 
   // Start Tact toolkit
   Tact.begin();
-  // Add new Sensor and config
   Tact.addSensor(SENSOR_ID, SPECTRUMSTART, SPECTRUMSTEPS, SPECTRUMSTEPSIZE);
+
+  // Start I2C
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
 }
 
 void loop() {
+  RunTactSensor();
+}
+
+// Read data in to buffer with the offset in first element, so we can use it in sendData
+void receiveData(int byteCount){
+  int counter = 0;
+  while(Wire.available()) {
+    receiveBuffer[counter] = Wire.read();
+    counter++;
+  }
+}
+
+// Use the offset value to select a function
+void sendData(){
+  float diff = abs(movingAvrg-baseline);
+  if(diff >= baselineTolerance) {
+    diff = abs(movingAvrg-baseline) - baselineTolerance;
+  }
+  else {
+    diff = 0.0f;
+  }
+
+  // If the buffer is set to 99, make some data
+  if (receiveBuffer[0] == 99) {
+    writeData(diff);
+  }
+  else if (receiveBuffer[0] == 98) {
+    writeData(2.222f);
+  }
+  else {
+    Serial.println("No function for this address");
+  }
+}
+
+// Write data
+void writeData(float newData) {
+  char dataString[8];
+  // Convert our data to a string/char[] with min length 5
+  dtostrf(newData, 5, 3, dataString);
+  Wire.write(dataString);
+}
+
+void RunTactSensor() {
   // read Spectrum for sensor
   // the spectrum array holds as many values as defined in SPECTRUMSTEPS
   Tact.readSpectrum(SENSOR_ID, spectrum);
@@ -61,18 +115,9 @@ void loop() {
   CalculateMovingAverage();
 
   // Calibrate in the first 5 seconds
-  if(millis() < 5000) {
+  if(millis() < 3000) {
     CalibrateSensor();
-  }
-
-  float diff = abs(movingAvrg-baseline);
-
-  Serial.print(F("Variation from baseline: "));
-  if(diff < baselineTolerance) {
-    Serial.println(0.0f);
-  }
-  else {
-    Serial.println(abs(movingAvrg-baseline) - baselineTolerance);
+    Serial.println("Calibrating");
   }
 }
 
